9b380a17cece80a46a9c549f434d12a3c814c49b
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-05-08 13:04:06 +0530

    update

---
+++ Array/A14.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums)
+{
+    int n = nums.size();
+
+    int index = -1;
+
+    // Step 1: Find breakpoint
+    for(int i = n - 2; i >= 0; i--)
+    {
+        if(nums[i] < nums[i + 1])
+        {
+            index = i;
+            break;
+        }
+    }
+
+    // If no breakpoint
+    if(index == -1)
+    {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    // Step 2: Find next greater element
+    for(int i = n - 1; i > index; i--)
+    {
+        if(nums[i] > nums[index])
+        {
+            swap(nums[i], nums[index]);
+            break;
+        }
+    }
+
+    // Step 3: Reverse remaining part
+    reverse(nums.begin() + index + 1, nums.end());
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3};
+
+    nextPermutation(nums);
+
+    cout << "Next Permutation: ";
+
+    for(int x : nums)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
