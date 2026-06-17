556fa16beb1e09eac69572a64900b48a3eec47de
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-17 15:48:55 +0530

    update

---
+++ Array/A48.cpp
@@ -0,0 +1,39 @@
+// ✅ Kadane's Algorithm
+// ✅ Maximum Subarray Sum
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubarraySum(vector<int> &arr) {
+
+        int currSum = arr[0];
+        int maxSum = arr[0];
+
+        for(int i = 1; i < arr.size(); i++) {
+
+            currSum = max(arr[i], currSum + arr[i]);
+
+            maxSum = max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    Solution obj;
+
+    cout << "Maximum Subarray Sum = "
+         << obj.maxSubarraySum(arr)
+         << endl;
+
+    return 0;
+}
