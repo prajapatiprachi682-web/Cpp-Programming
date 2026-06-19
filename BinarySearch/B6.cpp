5ee109d70f166d5d4fd28935cd7c072b82051877
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-19 09:05:12 +0530

    update

---
+++ BinarySearch/B6.cpp
@@ -0,0 +1,85 @@
+// ✅ Arrays
+// ✅ Binary Search
+// ✅ First Occurrence & Last Occurrence
+// ✅ Count Frequency in Sorted Array
+// ✅ GFG
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    int firstOcc(vector<int>& arr, int target) {
+
+        int low = 0, high = arr.size() - 1;
+        int ans = -1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int lastOcc(vector<int>& arr, int target) {
+
+        int low = 0, high = arr.size() - 1;
+        int ans = -1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return ans;
+    }
+
+    int countFreq(vector<int>& arr, int target) {
+
+        int first = firstOcc(arr, target);
+
+        if(first == -1)
+            return 0;
+
+        int last = lastOcc(arr, target);
+
+        return last - first + 1;
+    }
+};
+
+int main()
+{
+    vector<int> arr = {1,2,2,2,3,4};
+    int target = 2;
+
+    Solution obj;
+
+    cout << obj.countFreq(arr, target);
+
+    return 0;
+}
