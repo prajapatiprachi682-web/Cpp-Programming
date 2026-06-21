a0d2dfce0249946e648c2639c63d313cd5a74e15
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-21 15:56:16 +0530

    update

---
+++ BinarySearch/B7.cpp
@@ -0,0 +1,70 @@
+// ✅ Binary Search
+// ✅ Divide and Conquer
+// ✅ Two Sorted Arrays
+// ✅ Median/K-th Element Pattern
+// ✅ GFG - K-th Element of Two Sorted Arrays
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+
+        int n = a.size();
+        int m = b.size();
+
+        if(n > m)
+            return kthElement(b, a, k);
+
+        int low = max(0, k - m);
+        int high = min(k, n);
+
+        while(low <= high)
+        {
+            int cut1 = (low + high) / 2;
+            int cut2 = k - cut1;
+
+            int l1 = (cut1 == 0) ? INT_MIN : a[cut1 - 1];
+            int l2 = (cut2 == 0) ? INT_MIN : b[cut2 - 1];
+
+            int r1 = (cut1 == n) ? INT_MAX : a[cut1];
+            int r2 = (cut2 == m) ? INT_MAX : b[cut2];
+
+            if(l1 <= r2 && l2 <= r1)
+            {
+                return max(l1, l2);
+            }
+            else if(l1 > r2)
+            {
+                high = cut1 - 1;
+            }
+            else
+            {
+                low = cut1 + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    vector<int> a = {2,3,6,7,9};
+    vector<int> b = {1,4,8,10};
+
+    int k = 5;
+
+    Solution obj;
+
+    cout << obj.kthElement(a, b, k);
+
+    return 0;
+}
