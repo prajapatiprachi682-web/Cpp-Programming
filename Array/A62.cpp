c1066f3de9e85131909e3e335b80513e6b48bfbd
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-23 10:17:14 +0530

    Array

---
+++ Array/A62.cpp
@@ -0,0 +1,52 @@
+// ✅ Arrays
+// ✅ Dynamic Programming (Kadane Variant)
+// ✅ Maximum Product Subarray
+// ✅ LeetCode 152 - Maximum Product Subarray
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int ans = nums[0];
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] < 0)
+            {
+                swap(currMax, currMin);
+            }
+
+            currMax = max(nums[i],
+                          currMax * nums[i]);
+
+            currMin = min(nums[i],
+                          currMin * nums[i]);
+
+            ans = max(ans, currMax);
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {2,3,-2,4};
+
+    Solution obj;
+
+    cout << obj.maxProduct(nums);
+
+    return 0;
+}
