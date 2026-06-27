4750feb7f90a4a00c6618dc2ec19b07742d2d09b
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-06-27 10:28:35 +0530

    String

--- String/s6.cpp
+++ String/s6.cpp
@@ -1,1 +1,43 @@
-//
+// ✅ Strings
+// ✅ Nested Loops
+// ✅ Brute Force
+// ✅ Character Searching
+// ✅ GFG - First Repeating Character
+
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    char firstRep(string s)
+    {
+        int n = s.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(s[i] == s[j])
+                    return s[i];
+            }
+        }
+
+        return '#';
+    }
+};
+
+int main()
+{
+    string s = "geeksforgeeks";
+
+    Solution obj;
+
+    cout << obj.firstRep(s);
+
+    return 0;
+}
