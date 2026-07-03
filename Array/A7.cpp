6364201be5263c2a6b4c0fd108c52484c42be7eb
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-07-03 10:55:16 +0530

    Array

--- Array/A7.cpp
+++ Array/A7.cpp
@@ -1,43 +1,26 @@
-#include <iostream>
+// Swap Alternate Elements
+
+
+#include<iostream>
 using namespace std;
 
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = 5;
-    int product = 1;
+    int arr[6] = {10,20,30,40,50,60};
+
+    int n = 6;
+
+    for(int i = 0; i < n-1; i += 2)
+    {
+        swap(arr[i], arr[i+1]);
+    }
+
+    cout << "Swapped Array: ";
 
     for(int i = 0; i < n; i++)
     {
-        product = product * arr[i];
+        cout << arr[i] << " ";
     }
-
-    cout << "Product = " << product;
 
     return 0;
 }
-
-
-
-
-
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     cout << "Enter n: ";
-//     cin >> n;
-
-//     int arr[n];
-//     cout << "Enter elements:\n";
-//     for(int i = 0; i < n; i++) cin >> arr[i];
-
-//     int product = 1;
-//     for(int i = 0; i < n; i++) {
-//         product *= arr[i];
-//     }
-
-//     cout << "Product = " << product;
-//     return 0;
-// }
