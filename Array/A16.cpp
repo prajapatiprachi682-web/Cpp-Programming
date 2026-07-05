c16fe53701749c92cc6d350940e442c5f088754a
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-07-05 11:40:15 +0530

    Array

--- Array/A16.cpp
+++ Array/A16.cpp
@@ -1,19 +1,22 @@
-// 2️⃣ strcpy() → Copy String
+// Array
+// Find Middle Element
+
 
 
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-int main() 
-{
+int main() {
+    int arr[5]={5,4,9,4,5};
+    int sum=0;
+    int n=sizeof(arr)/sizeof(arr[0]);
 
-    char source[] = "Hello";
-    char destination[100];
-
-    strcpy(destination, source);
-
-    cout << "Copied String = " << destination;
-
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        
+    }
+    int mid=n/2;
+    cout<<arr[mid]<<endl;
+    
     return 0;
 }
