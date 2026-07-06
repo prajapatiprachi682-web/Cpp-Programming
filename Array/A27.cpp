82c7c83a8d2a8289621833251f74d79f087e2b35
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-07-06 13:11:39 +0530

    Array

--- Array/A27.cpp
+++ Array/A27.cpp
@@ -1,64 +1,37 @@
+// Array
+// Maximum Total Subarray Value I
+// ✅ #3689 — Maximum Total Subarray Value I
+
+
+
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot)
+    long long maxTotalValue(vector<int>& nums, int k)
     {
-        vector<int> less;
-        vector<int> same;
-        vector<int> greater;
+        int maxi = *max_element(nums.begin(), nums.end());
+        int mini = *min_element(nums.begin(), nums.end());
 
-        for(int num : nums)
-        {
-            if(num < pivot)
-            {
-                less.push_back(num);
-            }
-            else if(num == pivot)
-            {
-                same.push_back(num);
-            }
-            else
-            {
-                greater.push_back(num);
-            }
-        }
-
-        vector<int> ans;
-
-        for(int x : less)
-            ans.push_back(x);
-
-        for(int x : same)
-            ans.push_back(x);
-
-        for(int x : greater)
-            ans.push_back(x);
-
-        return ans;
+        return 1LL * k * (maxi - mini);
     }
 };
 
 int main()
 {
-    vector<int> nums = {9, 12, 5, 10, 14, 3, 10};
-    int pivot = 10;
+    vector<int> nums = {1, 3, 2};
+    int k = 2;
 
     Solution obj;
 
-    vector<int> ans = obj.pivotArray(nums, pivot);
-
-    cout << "Pivoted Array: ";
-
-    for(int x : ans)
-    {
-        cout << x << " ";
-    }
-
-    cout << endl;
+    cout << "Maximum Total Value = "
+         << obj.maxTotalValue(nums, k)
+         << endl;
 
     return 0;
 }
