189d58341860f397e7256d0771f57e52d72bc9e5
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-07-07 13:28:52 +0530

    Array

--- Array/A30.cpp
+++ Array/A30.cpp
@@ -1,104 +1,114 @@
+// Array
+// Monotonic Stack
+// Prefix Sum
+// Sum of Total Strength of Wizards
+// ✅ #2281 — Sum of Total Strength of Wizards
+
+
+
+
+
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <tuple>
-#include <algorithm>
-#include <climits>
+#include <stack>
 using namespace std;
 
 class Solution
 {
 public:
-    long long maxTotalValue(vector<int>& nums, int k)
+    int totalStrength(vector<int>& strength)
     {
-        int n = nums.size();
+        int n = strength.size();
+        long long MOD = 1000000007;
 
-        int logn = 32 - __builtin_clz(n);
-
-        vector<vector<int>> stMax(n, vector<int>(logn));
-        vector<vector<int>> stMin(n, vector<int>(logn));
+        vector<long long> pre(n + 1, 0);
+        vector<long long> ppre(n + 2, 0);
 
         for(int i = 0; i < n; i++)
         {
-            stMax[i][0] = nums[i];
-            stMin[i][0] = nums[i];
+            pre[i + 1] = (pre[i] + strength[i]) % MOD;
         }
 
-        for(int j = 1; j < logn; j++)
+        for(int i = 0; i <= n; i++)
         {
-            for(int i = 0; i + (1 << j) <= n; i++)
+            ppre[i + 1] = (ppre[i] + pre[i]) % MOD;
+        }
+
+        vector<int> left(n), right(n);
+
+        stack<int> st;
+
+        // Previous Smaller Element
+        for(int i = 0; i < n; i++)
+        {
+            while(!st.empty() &&
+                  strength[st.top()] >= strength[i])
             {
-                stMax[i][j] =
-                    max(stMax[i][j - 1],
-                        stMax[i + (1 << (j - 1))][j - 1]);
-
-                stMin[i][j] =
-                    min(stMin[i][j - 1],
-                        stMin[i + (1 << (j - 1))][j - 1]);
+                st.pop();
             }
+
+            left[i] = st.empty() ? -1 : st.top();
+
+            st.push(i);
         }
 
-        priority_queue<tuple<int, int, int>> pq;
-
-        for(int l = 0; l < n; l++)
+        while(!st.empty())
         {
-            int len = n - l;
-            int j = 31 - __builtin_clz(len);
+            st.pop();
+        }
 
-            int maxi =
-                max(stMax[l][j],
-                    stMax[n - (1 << j)][j]);
+        // Next Smaller Element
+        for(int i = n - 1; i >= 0; i--)
+        {
+            while(!st.empty() &&
+                  strength[st.top()] > strength[i])
+            {
+                st.pop();
+            }
 
-            int mini =
-                min(stMin[l][j],
-                    stMin[n - (1 << j)][j]);
+            right[i] = st.empty() ? n : st.top();
 
-            pq.push(make_tuple(maxi - mini, l, n - 1));
+            st.push(i);
         }
 
         long long ans = 0;
 
-        while(k--)
+        for(int i = 0; i < n; i++)
         {
-            tuple<int, int, int> temp = pq.top();
-            pq.pop();
+            int l = left[i];
+            int r = right[i];
 
-            int val = get<0>(temp);
-            int l   = get<1>(temp);
-            int r   = get<2>(temp);
+            long long leftCount = i - l;
+            long long rightCount = r - i;
 
-            ans += val;
+            long long leftSum =
+                (ppre[i + 1] - ppre[max(0, l + 1)] + MOD) % MOD;
 
-            if(r > l)
-            {
-                int len = r - l;
-                int j = 31 - __builtin_clz(len);
+            long long rightSum =
+                (ppre[r + 1] - ppre[i + 1] + MOD) % MOD;
 
-                int maxi =
-                    max(stMax[l][j],
-                        stMax[r - (1 << j)][j]);
+            long long total =
+                (rightSum * leftCount % MOD
+                - leftSum * rightCount % MOD
+                + MOD) % MOD;
 
-                int mini =
-                    min(stMin[l][j],
-                        stMin[r - (1 << j)][j]);
-
-                pq.push(make_tuple(maxi - mini, l, r - 1));
-            }
+            ans =
+                (ans +
+                1LL * strength[i] * total) % MOD;
         }
 
-        return ans;
+        return (int)ans;
     }
 };
 
 int main()
 {
-    vector<int> nums = {1, 3, 2};
-    int k = 2;
+    vector<int> strength = {1, 3, 1, 2};
 
     Solution obj;
 
-    cout << "Maximum Total Value = "
-         << obj.maxTotalValue(nums, k)
+    cout << "Total Strength = "
+         << obj.totalStrength(strength)
          << endl;
 
     return 0;
