37ccc3b0386a4355e5c89d310f1e7214d07a188f
Prachi Prajapati <prajapatiprachi682@gmail.com>
2026-07-07 13:34:51 +0530

    Array

--- Array/A32.cpp
+++ Array/A32.cpp
@@ -1,109 +1,42 @@
-//
+// Array
+// Minimum and Maximum in Array
+// Gfg/ Code360
+
 
 
 
 #include <iostream>
 #include <vector>
-#include <stack>
 using namespace std;
 
 class Solution
 {
 public:
-    int totalStrength(vector<int>& strength)
+    vector<int> getMinMax(vector<int> &arr)
     {
-        int n = strength.size();
-        long long MOD = 1000000007;
+        int mini = arr[0];
+        int maxi = arr[0];
 
-        vector<long long> pre(n + 1, 0);
-        vector<long long> ppre(n + 2, 0);
-
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i < arr.size(); i++)
         {
-            pre[i + 1] = (pre[i] + strength[i]) % MOD;
+            mini = min(mini, arr[i]);
+            maxi = max(maxi, arr[i]);
         }
 
-        for(int i = 0; i <= n; i++)
-        {
-            ppre[i + 1] = (ppre[i] + pre[i]) % MOD;
-        }
-
-        vector<int> left(n), right(n);
-
-        stack<int> st;
-
-        // Previous Smaller Element
-        for(int i = 0; i < n; i++)
-        {
-            while(!st.empty() &&
-                  strength[st.top()] >= strength[i])
-            {
-                st.pop();
-            }
-
-            left[i] = st.empty() ? -1 : st.top();
-
-            st.push(i);
-        }
-
-        while(!st.empty())
-        {
-            st.pop();
-        }
-
-        // Next Smaller Element
-        for(int i = n - 1; i >= 0; i--)
-        {
-            while(!st.empty() &&
-                  strength[st.top()] > strength[i])
-            {
-                st.pop();
-            }
-
-            right[i] = st.empty() ? n : st.top();
-
-            st.push(i);
-        }
-
-        long long ans = 0;
-
-        for(int i = 0; i < n; i++)
-        {
-            int l = left[i];
-            int r = right[i];
-
-            long long leftCount = i - l;
-            long long rightCount = r - i;
-
-            long long leftSum =
-                (ppre[i + 1] - ppre[max(0, l + 1)] + MOD) % MOD;
-
-            long long rightSum =
-                (ppre[r + 1] - ppre[i + 1] + MOD) % MOD;
-
-            long long total =
-                (rightSum * leftCount % MOD
-                - leftSum * rightCount % MOD
-                + MOD) % MOD;
-
-            ans =
-                (ans +
-                1LL * strength[i] * total) % MOD;
-        }
-
-        return (int)ans;
+        return {mini, maxi};
     }
 };
 
 int main()
 {
-    vector<int> strength = {1, 3, 1, 2};
+    vector<int> arr = {1, 4, 3, 5, 8, 6};
 
     Solution obj;
 
-    cout << "Total Strength = "
-         << obj.totalStrength(strength)
-         << endl;
+    vector<int> ans = obj.getMinMax(arr);
+
+    cout << "Minimum = " << ans[0] << endl;
+    cout << "Maximum = " << ans[1] << endl;
 
     return 0;
 }
